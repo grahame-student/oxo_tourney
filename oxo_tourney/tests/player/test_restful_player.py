f768e1a418278c232c529cff7502ea0b38b6caab
Grahame White <4whitg16@solent.ac.uk>
2022-06-27 22:23:31 +0100

    Add tests for RestfulPlayer

--- oxo_tourney/tests/player/test_restful_player.py
+++ oxo_tourney/tests/player/test_restful_player.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 from hamcrest import assert_that, equal_to
 from oxo_tourney.player import RestfulPlayer
@@ -9,3 +10,14 @@ class TestRestfulPlayer(TestCase):
         player = RestfulPlayer("some name")
 
         assert_that(player.name, equal_to("some name"))
+
+    def test_move_contains_two_elements(self):
+        # Not a good test case!
+        player = RestfulPlayer("")
+        board_mock = MagicMock()
+        board_mock.size = 2
+        board_mock.__str__.return_value = "...\n...\n..."
+
+        move = player.get_move(board_mock, "X")
+
+        assert_that(len(move), equal_to(2))
