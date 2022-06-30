8040c06efefc4d4dbf5158c2ba92d2bb645c7798
fading-paths-dev <108132319+fading-paths-dev@users.noreply.github.com>
2022-06-30 08:16:57 +0100

    Split get_move into two functions

--- oxo_tourney/player/kati_player.py
+++ oxo_tourney/player/kati_player.py
@@ -6,28 +6,36 @@ from oxo_tourney.player.player import Player
 # Add a line for your player class to the __init__.py file
 # Implement your version of the get_move function
 
+
+def get_position(board, symbol, size):
+    available_spaces = []
+    for var in range(size * size):
+        if board[var] == '.':
+            available_spaces.append(var)
+
+    return random.choice(available_spaces)
+
+
+def convert_position_to_row_and_col(element, size):
+    for var in range(size):
+        if element < (size * (var + 1)) and (element >= (size * var)):
+            row = element - (size * var)
+            col = var
+
+    return [row, col]
+
+
 class KatiPlayer(Player):
     def __init__(self, name):
         super().__init__(name)
 
     def get_move(self, board, symbol):
-        # fluff code for basic testing
         size = board.size
-        row = 0
-        col = 0
         board_state = f"{board}".splitlines()
         board_state = "".join(board_state)
 
-        available_spaces = []
-        for var in range(size * size):
-            if board_state[var] == '.':
-                available_spaces.append(var)
+        element = get_position(board_state, symbol, size)
 
-        element = random.choice(available_spaces)
-
-        for var in range(size):
-            if element < (size * (var + 1)) and (element >= (size * var)):
-                row = element - (size * var)
-                col = var
-
-        return [row, col]
+        move = convert_position_to_row_and_col(element, size)
+        print("kt")
+        return [move[0], move[1]]
