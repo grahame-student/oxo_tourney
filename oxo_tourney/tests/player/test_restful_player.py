f0b636142e384e4f1e587c5e03fc756b273def61
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-07-01 19:10:00 +0100

    Update test_restful_player.py

--- oxo_tourney/tests/player/test_restful_player.py
+++ oxo_tourney/tests/player/test_restful_player.py
@@ -8,16 +8,26 @@ from oxo_tourney.player import RestfulPlayer
 class TestRestfulPlayer(TestCase):
     def test_name_returns_value_passed_into_constructor(self):
         player = RestfulPlayer("some name")
-
         assert_that(player.name, equal_to("some name"))
 
     def test_move_contains_two_elements(self):
         # Not a good test case!
         player = RestfulPlayer("")
         board_mock = MagicMock()
-        board_mock.size = 2
+        board_mock.size = 3
         board_mock.__str__.return_value = "...\n...\n..."
 
         move = player.get_move(board_mock, "X")
 
         assert_that(len(move), equal_to(2))
+
+    def test_get_move_diplays_warning_when_grid_size_not_3(self):
+        # Not a good test case!
+        player = RestfulPlayer("")
+        board_mock = MagicMock()
+        board_mock.size = 2
+        board_mock.__str__.return_value = "...\n...\n..."
+
+        with unittest.mock.patch('builtins.print') as mocked_print:    
+            move = player.get_move(board_mock, "X")
+            assert mocked_print.mock_calls == [call('Rest API only supports 3x3 boards')]
