844fa563b203a6f2ff835a2f89706769132108c8
rameshk553 <ramesh.kasaragadda@gmail.com>
2022-07-04 12:16:14 +0100

    added player draw and opponent winning specific unit tests

--- oxo_tourney/tests/player/test_ai_player.py
+++ oxo_tourney/tests/player/test_ai_player.py
@@ -13,17 +13,36 @@ class TestAiPlayer(TestCase):
     def test_get_move_returns_a_list_with_two_elements(self):
         player = AiPlayer("some name")
         board_mock = MagicMock()
-        board_mock.size = 2
+        board_mock.size = 3
         board_mock.__str__.return_value = "...\n...\n..."
 
         move = player.get_move(board_mock, "X")
 
         assert_that(len(move), equal_to(2))
 
-    def test_get_move_returns_empty_list_when_board_is_full(self):
+    def test_get_move_returns_a_list_when_opponent_player_has_chance_to_win(self):
         player = AiPlayer("some name")
         board_mock = MagicMock()
-        board_mock.size = 2
+        board_mock.size = 3
+        board_mock.__str__.return_value = "XXO\n.OO\n..."
+        move = player.get_move(board_mock, "X")
+
+        assert_that(len(move), equal_to(2))
+
+    def test_get_move_returns_a_list_when_only_one_move_left_on_the_board(self):
+        player = AiPlayer("some name")
+        board_mock = MagicMock()
+        board_mock.size = 3
+        board_mock.__str__.return_value = "OXO\nOXO\n.OX"
+
+        move = player.get_move(board_mock, "X")
+
+        assert_that(len(move), equal_to(2))
+
+    def test_get_move_returns_an_empty_list_when_board_is_full(self):
+        player = AiPlayer("some name")
+        board_mock = MagicMock()
+        board_mock.size = 3
         board_mock.__str__.return_value = "XOX\nOXX\nOXO"
 
         move = player.get_move(board_mock, "X")
