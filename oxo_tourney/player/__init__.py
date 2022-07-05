5b9bd63560f60e01b118a5cb7f9ad6c2d2ca195c
rameshk553 <ramesh.kasaragadda@gmail.com>
2022-07-05 12:52:42 +0100

    oxo_tourney/player/__init__.py

--- oxo_tourney/player/__init__.py
+++ oxo_tourney/player/__init__.py
@@ -4,10 +4,4 @@ from .human_player import HumanPlayer
 from .random_player import RandomPlayer
 from .restful_player import RestfulPlayer
 
-__all__ = [
-    "ExamplePlayer",
-    "HumanPlayer",
-    "RandomPlayer",
-    "RestfulPlayer",
-    "AiPlayer"
-]
+__all__ = ["ExamplePlayer", "HumanPlayer", "RandomPlayer", "RestfulPlayer", "AiPlayer"]
